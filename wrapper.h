2826a6bc1c8e2fa23061a5a18b812c915286d237
MOZGIII <mike-n@narod.ru>
2023-07-22 16:43:05 -0300

    Add a comment on the unlikely define

--- wrapper.h
+++ wrapper.h
@@ -1,6 +1,10 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+// In the nvme-cli, the `unlikely` macro is defined at `nvme-cli/nvme.h` -
+// but we do not want to include that, so we inline it instead.
+// This might hve to be changed when the `nvme-cli` submodule is updated to
+// a newer version.
 #define unlikely(x) x
 
 #include "nvme-cli/linux/nvme.h"
